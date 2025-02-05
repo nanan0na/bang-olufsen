a176cdfefa6e76a78865f36a30bf37d9c5926285
nana <goldbarrr5@gmail.com>
2025-02-05 18:26:10 +0900

    header, visual cursor-ing

---
+++ js/common.js
@@ -0,0 +1,24 @@
+$(document).on('mousemove', function (e) {
+  var xPos = e.pageX;
+  var yPos = e.pageY;
+  console.log(xPos, yPos);
+  $('.cursor').css({
+    top: yPos,
+    left: xPos,
+  });
+
+  // `.visual` 영역 안에서만 커서 크기 조절
+  $('.visual').on('mouseenter', function () {
+    $('.cursor').css({
+      transform: 'scale(1)', // 원래 크기
+      opacity: '1', // 완전히 보이게
+    });
+  });
+
+  $('.visual').on('mouseleave', function () {
+    $('.cursor').css({
+      transform: 'scale(0.1)', // 작아지게
+      opacity: '0', // 살짝 투명하게
+    });
+  });
+});
